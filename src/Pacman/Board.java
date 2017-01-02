7c61642d91ef60435c8d83a1ac30c732f55bb18e
Akhil Trehan <Akhil@Akhils-MacBook-Pro.local>
2017-01-02 17:43:59 -0500

    imported cs015.jar

---
+++ src/Pacman/Board.java
@@ -0,0 +1,14 @@
+package Pacman;
+
+import cs015.fnl.PacmanSupport.SupportMap;
+
+public class Board {
+	private cs015.fnl.PacmanSupport.SupportMap _stencilMap;
+	
+	public Board(){
+		_stencilMap = new cs015.fnl.PacmanSupport.SupportMap();
+	}
+	
+
+	
+}
---
+++ src/Pacman/SmartSquare.java
@@ -0,0 +1,19 @@
+package Pacman;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class SmartSquare {
+	private Rectangle _rect;
+	
+	public SmartSquare(){
+		_rect.setWidth(20);
+		_rect.setHeight(20);
+		_rect.setStroke(Color.BLACK);
+	}
+	
+	public void squareContains(){
+		
+	}
+
+}
