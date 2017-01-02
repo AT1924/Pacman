0051041eb220a0e94f1f0e8ebc875ee953ddf1ea
Akhil Trehan <Akhil@Akhils-MacBook-Pro.local>
2017-01-02 18:38:36 -0500

    created board

--- src/Pacman/Board.java
+++ src/Pacman/Board.java
@@ -1,12 +1,27 @@
 package Pacman;
 
+import cs015.fnl.PacmanSupport.BoardLocation;
 import cs015.fnl.PacmanSupport.SupportMap;
 
 public class Board {
-	private cs015.fnl.PacmanSupport.SupportMap _stencilMap;
+	public BoardLocation[][] _map ;
 	
 	public Board(){
-		_stencilMap = new cs015.fnl.PacmanSupport.SupportMap();
+		cs015.fnl.PacmanSupport.SupportMap	_stencilMap = new cs015.fnl.PacmanSupport.SupportMap();
+		_map = _stencilMap.getMap();
+	}
+	
+	public static void main(String[] argv){
+		Board b = new Board();
+		for (int i = 0; i < b._map.length; i++){
+			for (int j = 0; j < b._map[i].length; j++){
+			System.out.println(b._map[i][j].values());	
+			// create smartsquare
+			// use information from b._map 
+			// insert smartsquare into board
+			// 
+			}
+		}
 	}
 	
 
--- src/Pacman/SmartSquare.java
+++ src/Pacman/SmartSquare.java
@@ -1,15 +1,18 @@
 package Pacman;
 
+import cs015.fnl.PacmanSupport.BoardLocation;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class SmartSquare {
 	private Rectangle _rect;
+	private BoardLocation _location;
 	
-	public SmartSquare(){
+	public SmartSquare(BoardLocation location){
 		_rect.setWidth(20);
 		_rect.setHeight(20);
 		_rect.setStroke(Color.BLACK);
+		_location = location;
 	}
 	
 	public void squareContains(){
