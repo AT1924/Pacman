f8969a419f85f1082a03c6dcb38fbfc92d0c6d7e
Akhil Trehan <Akhil@Akhils-MacBook-Pro.local>
2017-01-05 13:46:56 -0500

    Trying to insert Squares

---
+++ src/Pacman/Constants.java
@@ -0,0 +1,8 @@
+package Pacman;
+
+public class Constants {
+
+	public static final int NUM_SQUARES_Y = 22;
+	public static final int NUM_SQUARES_X = 22;
+	public static final int SQUARE_SIZE = 20;
+}
